5f6af3a244bad2001e41d7cf8e7eec3967e2a468
pokhmillya <zxcshadowelow@gmail.com>
2026-02-27 13:05:48 +0200

    добавил меню и сделал чтобы можно было создавать куча задач и выводить их тип

--- src/main.rs
+++ src/main.rs
@@ -23,28 +23,58 @@ impl Todo{
 }
 
 fn main() {
-    println!("Введи название");
-    let mut name_buf = String::new();
-    io::stdin()
-        .read_line(&mut name_buf).expect("Ошибка чтения");
-    let name = name_buf.trim();
+    let mut tasks: Vec<Todo> = Vec::new();
+    loop {
+        println!("=== Todo list ===");
+        println!("1. Добавить задачу");
+        println!("2. Показать все задачи");
+        println!("3. Выход");
+        
+        let mut action = String::new();
+        io::stdin().read_line(&mut action).expect("Ничо не ясно");
+        let action = action.trim();
 
-    println!("Введи описание");
-    let mut bio_buf = String::new();
-    io::stdin()
-        .read_line(&mut bio_buf)
-        .expect("Ошибка чтения био");
-    let bio = bio_buf.trim();
+        match action {
+            "1" => {
+                println!("Введи название");
+                let mut name_buf = String::new();
+                io::stdin()
+                    .read_line(&mut name_buf).expect("Ошибка чтения");
+                let name = name_buf.trim();
 
-    println!("Введи дни");
-    let mut deadline_buf = String::new();
-    io::stdin()
-        .read_line(&mut deadline_buf)
-        .expect("Не понятно");
-    let deadline: i32 = deadline_buf.trim().parse().expect("Это не число!");
+                println!("Введи описание");
+                let mut bio_buf = String::new();
+                io::stdin()
+                    .read_line(&mut bio_buf)
+                    .expect("Ошибка чтения био");
+                let bio = bio_buf.trim();
 
-    let task = Todo::new(name,bio,deadline);
-    task.info();
+                println!("Введи дни");
+                let mut deadline_buf = String::new();
+                io::stdin()
+                    .read_line(&mut deadline_buf)
+                    .expect("Не понятно");
+                let deadline: i32 = deadline_buf.trim().parse().expect("Это не число!");
 
+                let task = Todo::new(name,bio,deadline);
+                tasks.push(task);
+                println!("Задача добавлена");
+            },
+
+            "2" => {
+                for task in &tasks{
+                    task.info();
+                }
+            },
+
+            "3" => {
+                println!("Пока");
+                break;
+            },
+
+            _ => println!("Не известная команда"),
+        }
+
+    }
 
 }
